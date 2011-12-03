ed6ac22530d55738bb926fad37747c1796b95927
Gustavo Pastorelli <gpastorelli89@gmail.com>
2011-12-03 12:50:24 -0430

    agregar unos urls

--- urls.py
+++ urls.py
@@ -1,10 +1,18 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
+from Guardabosques.views import hello, current_date, hours_ahead
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+
 urlpatterns = patterns('',
+    ('^hello/$',hello),
+    ('^time/$',current_date),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    (r'%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'),
+     'django.views.static.serve', {'document_root' : settings.STATIC_ROOT })
     # Examples:
     # url(r'^$', 'pepe.views.home', name='home'),
     # url(r'^pepe/', include('pepe.foo.urls')),
