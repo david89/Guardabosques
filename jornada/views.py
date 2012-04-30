c3ab51d1d5366289e3803344f35d998c6c277aa8
Alfonso Ros <alfonsoros88@gmail.com>
2012-04-29 20:26:26 -0430

    Agregue el template que lista las jornadas a los usuarios normales

--- jornada/views.py
+++ jornada/views.py
@@ -1,10 +1,17 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from jornada.models import Jornada, ConstituidaPor
+from usuario.models import Perfil
 
+@staff_member_required
 def jornadas_pendientes(request):
+    """
+    Lista las jornadas pertenecientes de los usuarios.
+    """
     plantilla = u'jornada/jornadas_pendientes.html'
     jornadas = Jornada.objects.filter(estado=u'P')
     if request.method == 'POST':
@@ -19,4 +26,16 @@ def descripcion_jornada(request, identificador):
     j = Jornada.objects.get(id=identificador)
     detalles = ConstituidaPor.objects.filter(jornada=j)
     return render_to_response(plantilla, {u'detalles' : detalles},
+                              context_instance=RequestContext(request))
+
+@login_required
+def administrar_jornadas(request):
+    """
+    Lista las jornadas que posee el usuario.
+    """
+    plantilla = u'jornada/jornadas_de_trabajo.html'
+    perfil_usuario = Perfil.objects.get(usuario__id=request.user.pk)
+    jornadas = Jornada.objects.filter(perfil__id=perfil_usuario.pk)
+    return render_to_response(plantilla, {u'jornadas' : jornadas, 
+                                          u'tipo' : "pendiente"},
                               context_instance=RequestContext(request))
