eeb6c6a8c939973aaf246a8d8cdae9edd2e2b17b
Alfonso Ros <alfonsoros88@gmail.com>
2012-05-10 23:41:48 -0430

    Agregue el widget datepicker en agregar jornada
    El widget se activa solamente sobre el atributo 'fecha'

--- jornada/forms.py
+++ jornada/forms.py
@@ -4,25 +4,41 @@
 Formulario para agregar una jornada
 """
 
+import floppyforms as forms
 from django.forms.fields import ChoiceField
 from django.forms import RadioSelect
-from django.forms import ModelForm, Form
+from datetime import date
 
 from Guardabosques.jornada.models import Jornada
 from Guardabosques.jornada.models import ESTADO_JORNADA
 
+class SelecFecha(forms.DateInput):
+    """ Esta clase es para el widget del calendario """
+    template_name = 'floppyforms/SelecFecha.html'
 
-class FormularioEstadoJornada(Form):
+    class Meta:
+        js = (
+            'js/jquery-ui-1.8.17.custom/js/jquery-ui-1.8.17.custom.min.js',
+            'js/jquery-ui-1.8.17.custom/js/jquery-1.7.1.min.js',
+        )
+        css = {
+            'all' : (
+                'js/jquery-ui-1.8.17.custom/css/south-street/jquery-ui-1.8.17.custom.css',
+            )        
+        }
+
+class FormularioEstadoJornada(forms.Form):
     """ Formulario para el estado de las jornadas """
     estado = ChoiceField(widget=RadioSelect, choices=ESTADO_JORNADA)
 
-class FormularioJornada(ModelForm):
+class FormularioJornada(forms.ModelForm):
     """ Formulario para agregar una jornada """
+    fecha = forms.DateField(initial=date.today, widget=SelecFecha)
+
     def clean(self):
         datos = super(FormularioJornada, self).clean()
         return datos
 
     class Meta:
         model = Jornada
-        exclude = (u'estado', u'perfil')
-
+        exclude = (u'estado', u'perfil', u'actividades')
